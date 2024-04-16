3a0721fccddaa42bcde457c0127533364a4faf07
miulerbm <miulerbm00@gmail.com>
2024-04-16 10:19:51 -0500

    Admin Products Page

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,23 @@
+import { Nav, NavLink } from "@/components/Nav";
+
+export const dynamic = "force-dynamic"; //for having all info updated always and disable cache
+
+const AdminLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <>
+      <Nav>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/products">Products</NavLink>
+        <NavLink href="/admin/users">Customers</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
+      </Nav>
+      <div className="container my-6">{children}</div>
+    </>
+  );
+};
+
+export default AdminLayout;
